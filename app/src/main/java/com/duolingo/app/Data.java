26ff12e2c85dd30970c9a3dbd448c6077419b9f4
jorge-tabuenca <jorgetabuenca99@gmail.com>
2020-12-08 18:17:07 +0100

    Actualizar librerias

---
+++ app/src/main/java/com/duolingo/app/Data.java
@@ -0,0 +1,13 @@
+package com.duolingo.app;
+
+import java.util.ArrayList;
+
+public class Data {
+
+    // Variables estaticas que se comparten entre toda la APP
+
+    public static String serverIP = "192.168.56.1";
+
+    public static ArrayList<String> arrayCourses;
+
+}
